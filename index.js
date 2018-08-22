d38713a1671fbf6e6c81d7e024a1ab79aee0e4c6
Hieupv <hieupv@codersvn.com>
2018-08-22 10:34:30 +0700

    add exception

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { Auth } from './src/Services/Facades/Auth';
 export { Request } from './src/Services/Facades/Request';
 export { RequestParser } from './src/Services/RequestParser';
+export * from './Exceptions';
