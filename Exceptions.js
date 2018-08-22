8d53e66559a213e80529886c7ebb768c46723571
Hieupv <hieupv@codersvn.com>
2018-08-22 10:36:13 +0700

    fix single qoute

--- Exceptions.js
+++ Exceptions.js
@@ -1,41 +1,19 @@
-export {
-  AccessDeniedHttpException
-} from "./src/Exceptions/AccessDeniedHttpException";
-export {
-  BadRequestHttpException
-} from "./src/Exceptions/BadRequestHttpException";
-export { ConflictHttpException } from "./src/Exceptions/ConflictHttpException";
-export { Exception } from "./src/Exceptions/Exception";
-export { ExceptionHandler } from "./src/Exceptions/exceptionHandler";
-export { GoneHttpException } from "./src/Exceptions/GoneHttpException";
-export { HttpException } from "./src/Exceptions/HttpException";
-export { HTTP_SEE_OTHER } from "./src/Exceptions/HttpStatusCode";
-export { LengthAwarePaginator } from "./Utils/LengthAwarePaginator";
-export {
-  MethodNotAllowedHttpException
-} from "./src/Exceptions/MethodNotAllowedHttpException";
-export {
-  NotAcceptableHttpException
-} from "./src/Exceptions/NotAcceptableHttpException";
-export { NotFoundException } from "./src/Exceptions/NotFoundException";
-export {
-  PermissionDeniedException
-} from "./src/Exceptions/PermissionDeniedException";
-export {
-  PreconditionFailedHttpException
-} from "./src/Exceptions/PreconditionFailedHttpException";
-export {
-  PreconditionRequiredHttpException
-} from "./src/Exceptions/PreconditionRequiredHttpException";
-export {
-  ServiceUnavailableHttpException
-} from "./src/Exceptions/ServiceUnavailableHttpException";
-export {
-  TooManyRequestsHttpException
-} from "./src/Exceptions/TooManyRequestsHttpException";
-export {
-  UnauthorizedHttpException
-} from "./src/Exceptions/UnauthorizedHttpException";
-export {
-  UnsupportedMediaTypeHttpException
-} from "./src/Exceptions/UnsupportedMediaTypeHttpException";
+export { AccessDeniedHttpException } from './src/Exceptions/AccessDeniedHttpException';
+export { BadRequestHttpException } from './src/Exceptions/BadRequestHttpException';
+export { ConflictHttpException } from './src/Exceptions/ConflictHttpException';
+export { Exception } from './src/Exceptions/Exception';
+export { ExceptionHandler } from './src/Exceptions/exceptionHandler';
+export { GoneHttpException } from './src/Exceptions/GoneHttpException';
+export { HttpException } from './src/Exceptions/HttpException';
+export { HTTP_SEE_OTHER } from './src/Exceptions/HttpStatusCode';
+export { LengthAwarePaginator } from './Utils/LengthAwarePaginator';
+export { MethodNotAllowedHttpException } from './src/Exceptions/MethodNotAllowedHttpException';
+export { NotAcceptableHttpException } from './src/Exceptions/NotAcceptableHttpException';
+export { NotFoundException } from './src/Exceptions/NotFoundException';
+export { PermissionDeniedException } from './src/Exceptions/PermissionDeniedException';
+export { PreconditionFailedHttpException } from './src/Exceptions/PreconditionFailedHttpException';
+export { PreconditionRequiredHttpException } from './src/Exceptions/PreconditionRequiredHttpException';
+export { ServiceUnavailableHttpException } from './src/Exceptions/ServiceUnavailableHttpException';
+export { TooManyRequestsHttpException } from './src/Exceptions/TooManyRequestsHttpException';
+export { UnauthorizedHttpException } from './src/Exceptions/UnauthorizedHttpException';
+export { UnsupportedMediaTypeHttpException } from './src/Exceptions/UnsupportedMediaTypeHttpException';
